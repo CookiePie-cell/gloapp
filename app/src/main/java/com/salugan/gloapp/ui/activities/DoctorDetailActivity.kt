fba92685f81801ec280fe17b25f78a7325119905
Julio Nicholas <juliojnicolas1@gmail.com>
2023-06-09 13:52:25 +0800

    create empty doctor detail activity

---
+++ app/src/main/java/com/salugan/gloapp/ui/activities/DoctorDetailActivity.kt
@@ -0,0 +1,16 @@
+package com.salugan.gloapp.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.salugan.gloapp.databinding.ActivityDoctorDetailBinding
+
+class DoctorDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDoctorDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDoctorDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
